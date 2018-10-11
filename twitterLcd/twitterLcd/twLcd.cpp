d2a378a8d1620c0c9335e77360198718abe7636b
Federico Tondi <fedetondi@gmail.com>
2018-10-11 13:53:15 -0300

    twLcd

--- twitterLcd/twitterLcd/twLcd.cpp
+++ twitterLcd/twitterLcd/twLcd.cpp
@@ -1,6 +1,7 @@
 #include "twLcd.h"
 
-
+#define MAX_SPEED	5
+#define MIN_SPEED	1
 
 twLcd::twLcd()
 {
--- twitterLcd/twitterLcd/twLcd.h
+++ twitterLcd/twitterLcd/twLcd.h
@@ -1,10 +1,28 @@
 #pragma once
+#include "basicLCD.h"
+#include <vector>
+#include <string>
 class twLcd
 {
 public:
 	twLcd();
+	void showNextTwit();
+	void showAgain();
+	void showPrevious();
+	void incSpeed();
+	void decSpeed();
+	bool update();
+	void closeLcd();
 	~twLcd();
+private:
+	unsigned char tickCount;
+	unsigned char currentSpeed;
+	std::string currentTwit;
+	std::string currentTwitInfo;
+	basicLCD * lcd;
+	vector<> &list;
+	unsigned int currentTwit;
 };
 
-#include "basicLCD.h"
+
 
