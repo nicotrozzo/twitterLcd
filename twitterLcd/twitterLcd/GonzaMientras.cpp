2f5a939ccdf759b647044e0aa4df4e44d36e6f0c
gonzalo Davidov <gdavidov@itba.edu.ar>
2018-10-12 00:52:55 -0300

    A

--- twitterLcd/twitterLcd/GonzaMientras.cpp
+++ twitterLcd/twitterLcd/GonzaMientras.cpp
@@ -1,5 +1,22 @@
 #include "twLcd.h"
 
+#define MAX_SPEED	5
+#define MIN_SPEED	1
+#define DEFAULT_SPEED 3
+
+twLcd::twLcd(basicLCD* dispPointer, unsigned char totalTwits_)
+{
+	currentSpeed = DEFAULT_SPEED;
+	tickCount = 0;
+	lcd = dispPointer;
+	totalTwits = totalTwits_;
+	parser();							//falta parsear fecha y hora
+
+	twitIndex = 0;
+	showAgain();
+}
+
+
 void twLcd :: showNextTwit()
 {
 	if (twitIndex < totalTwits)
@@ -44,7 +61,6 @@ void twLcd::closeLcd()
 	lcd->lcdClear;
 }
 
-
 void twLcd::showTwit()
 {
 	tickCount = 0;								//Reinicia el timer
@@ -52,13 +68,84 @@ void twLcd::showTwit()
 	currentTwitData = list[twitIndex].data;
 	lcd->lcdClear;
 	(*lcd) << currentTwitData.c_str();
-	lcd->lcdSetCursorPosition[2][1];
+	lcd->lcdSetCursorPosition[2][1];			//Llevo cursor a la posicion 1 de el segundo renglon
 	(*lcd) << currentTwit.c_str();
 }
 
-
+void twLcd::parser()
+{
+	for (unsigned char i = 0; i < totalTwits; i++)
+	{
+		for (unsigned char j = 0; j < list[i].text.size; j++)
+		{
+			switch (list[i].text[j])
+			{
+				case 'á':
+				{
+					list[i].text[j] = 'a';
+					break;
+				}
+				case 'é':
+				{
+					list[i].text[j] = 'e';
+					break;
+				}
+				case 'í':
+				{
+					list[i].text[j] = 'i';
+					break;
+				}
+				case 'ó':
+				{
+					list[i].text[j] = 'o';
+					break;
+				}
+				case 'ú':
+				{
+					list[i].text[j] = 'u';
+					break;
+				}
+				case 'Á':
+				{
+					list[i].text[j] = 'A';
+					break;
+				}
+				case 'É':
+				{
+					list[i].text[j] = 'E';
+					break;
+				}
+				case 'Í':
+				{
+					list[i].text[j] = 'I';
+					break;
+				}
+				case 'Ó':
+				{
+					list[i].text[j] = 'O';
+					break;
+				}
+				case 'Ú':
+				{
+					list[i].text[j] = 'U';
+					break;
+				}
+				case 'ñ':
+				{
+					list[i].text[j] = 'n';
+					break;
+				}
+				case 'Ñ':
+				{
+					list[i].text[j] = 'N';
+					break;
+				}
+			}
+		}
+	}
+}
 
 twLcd::~twLcd()
 {
-	
+	closeLcd;
 }
