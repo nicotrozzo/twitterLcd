6587707c6819da4522573c15c0ebc8134da2579d
gonzalo Davidov <gdavidov@itba.edu.ar>
2018-10-13 20:11:49 -0300

    AA

--- twitterLcd/twitterLcd/main.cpp
+++ twitterLcd/twitterLcd/main.cpp
@@ -5,13 +5,19 @@
 #define DISPLAY 4
 #define DEFAULT_AMOUNT_OF_TWITS 15
 
-int main(int argc,char *argv[])
+bool cmdLineParser(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
 {
 	cmdLineParser(argc, argv);
 	twUser user(argv[1], atoi(argv[2]));
 	if (user.getError().type == NO_TW_ERR)
 	{
-		user.getTwits();
+		while (user.getTwits() != 0)
+		{
+			//dibujar en el lcd algo para que se vea que no se colgo
+		}
+
 	}
 }
 
